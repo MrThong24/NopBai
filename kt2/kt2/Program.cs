71af9c79d710db13356eefcab4a144e7e5a269ef
MrThong24 <84189017+MrThong24@users.noreply.github.com>
2022-04-26 07:54:44 +0700

    uoload

---
+++ kt2/kt2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat263 = new Cat();
+            cat263.eat();
+            cat263.makeSound();
+            cat263.run();
+            Bird bird = new Bird();
+            bird.eat();
+            bird.makeSound();
+            bird.fly();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Foot");
+        }
+        public virtual void makeSound()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Cat : Animal
+    {
+        public void run()
+        {
+            Console.WriteLine("Meo chay nhanh");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Meo an ca");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Meo keu meo meo");
+        }
+    }
+    class Bird : Animal
+    {
+        public void fly()
+        {
+            Console.WriteLine("Chim bay cao");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Chim an sau");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Chim an con trung");
+        }
+    }
+}
